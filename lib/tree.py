561d234a753e099813ace30ab95ef90f85e3d073
RegisLewis <regisaslewis@gmail.com>
2023-09-19 16:16:42 -0700

    tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    ids = []
+    nodes_to_check = [n for n in self.root["children"]]
+
+    while len(nodes_to_check) > 0:
+      node = nodes_to_check.pop(0)
+      ids.append(node)
+      nodes_to_check = node["children"] + nodes_to_check
+
+    for n in ids:
+      if n["id"] == id:
+        return n
+    
+    return None
